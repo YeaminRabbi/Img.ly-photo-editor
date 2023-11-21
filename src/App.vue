be325e51d69a337e0433d0b744ed85202ab9537c
yeaminrabbi <yeaminrabbi308@gmail.com>
2023-11-21 17:03:00 +0600

    img.ly api integration - local - trail access

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <CreativeEditorSDK
+      :config="{
+        callbacks: { onUpload: 'local' } // Enables local uploads in Asset Library. 
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import CreativeEditorSDK from './components/CreativeEditorSDK.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CreativeEditorSDK
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
